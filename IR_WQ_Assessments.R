4d4fa2a81593a5e48a10e5f879e4cbf4f52a1e33
marybecker <mary.becker@ct.gov>
2019-10-23 11:43:44 -0400

    start - pull data from NWIS with dataRet

---
+++ IR_WQ_Assessments.R
@@ -0,0 +1,23 @@
+library(dataRetrieval)
+library(leaflet)
+
+pcd<-"50468"
+state<-"CT"
+start.date <-"2017-01-01"
+end.date <- "2018-12-31"
+stype <- "ST"
+
+#Get all stream sites that have data with param cd and within dates
+bactiCTsites<- readNWISdata(stateCd=state,parameterCd=pcd,startDate=start.date,endDate=end.date,
+                            siteType=stype,service="site")
+#Map of sites
+leaflet(data=bactiCTsites) %>%
+  addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+  addCircleMarkers(~dec_long_va,~dec_lat_va,color = "red", radius=5, stroke=FALSE,
+                   fillOpacity = 0.8, opacity = 0.8, popup=~station_nm)
+
+#Get site no to get data
+sites<-bactiCTsites$site_no
+
+#PUll data for sites and dates
+bactiCT<- readNWISqw(siteNumbers=sites,parameterCd=pcd,startDate=start.date,endDate=end.date)
